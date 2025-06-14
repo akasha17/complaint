ae12ad34daa32b7932e624c7ea1edbb262c19136
akash <akasha17828848@gmail.com>
2025-06-14 15:52:01 +0530

    Initial commit - Complaint Management System

---
+++ ComplaintManagement-Django/ComplaintMS/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class SuitConfig(AppConfig):
+    name = 'ComplaintMS'
+    verbose_name = "Complaint Management"
+
+class ComplaintMSConfig(AppConfig):
+    name = 'ComplaintMS'
+    
